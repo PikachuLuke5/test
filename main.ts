00684a8c24507d21991f4c07a3dee1d691c97851
PikachuLuke5 <142318212+PikachuLuke5@users.noreply.github.com>
2024-06-22 13:51:59 -0400

    Made game overs and score!

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+info.onScore(1000, function () {
+    sprites.destroy(mySprite)
+    mySprite2.sayText("Game over!")
+})
 controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
     mySprite.y += -2
 })
@@ -7,10 +11,17 @@ controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
 controller.down.onEvent(ControllerButtonEvent.Repeated, function () {
     mySprite.y += 2
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(5)
+})
 controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
     mySprite.x += -2
 })
+let mySprite2: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(assets.image`Player`, SpriteKind.Player)
-scene.setBackgroundImage(assets.image`red`)
+mySprite2 = sprites.create(assets.image`myImage0`, SpriteKind.Food)
+mySprite.x = 20
 music.play(music.createSong(assets.song`song`), music.PlaybackMode.UntilDone)
+scene.setBackgroundImage(assets.image`red`)
+mySprite.setBounceOnWall(true)
